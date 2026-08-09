d68cd46d2447a770b4f9bb3d5ca867307961d259
Galymzhan Ayapbergen <gaayapbergen@gmail.com>
2026-08-09 18:27:47 +0500

    test: add user login endpoint integration tests

---
+++ backend/tests/integration/test_user_login_endpoint.py
@@ -0,0 +1,183 @@
+from uuid import uuid4
+
+import pytest
+from fastapi import status
+from httpx import ASGITransport, AsyncClient
+from sqlalchemy import delete
+
+from app.database.session import async_session_factory
+from app.main import app
+from app.security.password import hash_password
+from app.security.tokens import decode_access_token
+from app.users.model import User
+
+
+async def create_test_user(
+    email: str,
+    password: str,
+    is_blocked: bool = False,
+) -> User:
+    async with async_session_factory() as session, session.begin():
+        user = User(
+            email=email, hashed_password=hash_password(password), is_blocked=is_blocked
+        )
+
+        session.add(user)
+
+        # flush executes insert not ending transaction
+        await session.flush()
+        # refresh loads database-generated data (id, timestamps)
+        await session.refresh(user)
+        # session.begin() commits automatically if there is not error, else rollback
+
+        return user
+
+
+async def delete_test_user(
+    email: str,
+) -> None:
+    async with async_session_factory() as session, session.begin():
+        await session.execute(delete(User).where(User.email == email))
+
+
+@pytest.mark.integration
+@pytest.mark.asyncio(loop_scope="session")
+async def test_successful_login() -> None:
+    # Arrange
+    email = f"user-{uuid4()}@example.com"
+    password = "strong-password"
+
+    created_user = await create_test_user(email, password)
+
+    transport = ASGITransport(app=app)
+
+    try:
+        async with AsyncClient(
+            transport=transport,
+            base_url="http://test",
+        ) as client:
+            # Act
+            response = await client.post(
+                "/api/v1/auth/login",
+                json={
+                    "email": email,
+                    "password": password,
+                },
+            )
+
+            response_data = response.json()
+
+        # Assert
+        assert response.status_code == status.HTTP_200_OK
+        assert response_data["token_type"] == "bearer"
+        assert isinstance(response_data["access_token"], str)
+
+        token_subject = decode_access_token(
+            response_data["access_token"],
+        )
+
+        assert token_subject == created_user.id
+
+    finally:
+        await delete_test_user(email)
+
+
+@pytest.mark.integration
+@pytest.mark.asyncio(loop_scope="session")
+async def test_login_with_unknown_email() -> None:
+    # Arrange
+    email = f"user-{uuid4()}@example.com"
+    password = "strong-password"
+
+    transport = ASGITransport(app=app)
+
+    # Act
+    async with AsyncClient(
+        transport=transport,
+        base_url="http://test",
+    ) as client:
+        response = await client.post(
+            "/api/v1/auth/login", json={"email": email, "password": password}
+        )
+
+        response_data = response.json()
+
+    # Assert
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response_data == {"detail": "Invalid email or password"}
+    assert response.headers["WWW-Authenticate"] == "Bearer"
+
+
+@pytest.mark.integration
+@pytest.mark.asyncio(loop_scope="session")
+async def test_login_with_wrong_password() -> None:
+    # Arrange
+    email = f"user-{uuid4()}@example.com"
+    password = "strong-password"
+
+    await create_test_user(
+        email=email,
+        password=password,
+    )
+
+    transport = ASGITransport(app=app)
+
+    # Act
+    try:
+        async with AsyncClient(transport=transport, base_url="http://test") as client:
+            response = await client.post(
+                "/api/v1/auth/login",
+                json={
+                    "email": email,
+                    "password": "incorrect-password",
+                },
+            )
+
+            response_data = response.json()
+
+        # Assert
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        assert response_data == {"detail": "Invalid email or password"}
+        assert response.headers["WWW-Authenticate"] == "Bearer"
+    finally:
+        await delete_test_user(email)
+
+
+@pytest.mark.integration
+@pytest.mark.asyncio(loop_scope="session")
+async def test_login_blocked_user() -> None:
+    # Arrange
+    email = f"user-{uuid4()}@example.com"
+    password = "strong-password"
+
+    transport = ASGITransport(app=app)
+
+    await create_test_user(
+        email=email,
+        password=password,
+        is_blocked=True,
+    )
+
+    # Act
+    try:
+        async with AsyncClient(
+            transport=transport,
+            base_url="http://test",
+        ) as client:
+            response = await client.post(
+                "/api/v1/auth/login",
+                json={
+                    "email": email,
+                    "password": password,
+                },
+            )
+
+            response_data = response.json()
+
+        # Assert
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+        assert response_data == {"detail": "User account is blocked"}
+        assert "access_token" not in response_data
+
+    finally:
+        await delete_test_user(email=email)
