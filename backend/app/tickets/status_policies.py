1390fb2c5c11eb15c91467c0fb1c7a47e553fb3d
Galymzhan Ayapbergen <gaayapbergen@gmail.com>
2026-08-24 17:26:09 +0500

    feat: add customer ticket status policy

---
+++ backend/app/tickets/status_policies.py
@@ -0,0 +1,31 @@
+from app.tickets.model import Ticket, TicketStatus
+from app.users.model import User
+
+
+class CustomerTicketStatusPolicy:
+    def has_access(
+        self,
+        ticket: Ticket,
+        user: User,
+    ) -> bool:
+        return user.id == ticket.customer_id
+
+    def allowed_statuses(
+        self,
+        ticket: Ticket,
+        user: User,
+    ) -> set[TicketStatus]:
+        current_status = ticket.status
+
+        if current_status is TicketStatus.OPEN:
+            return {
+                TicketStatus.CLOSED,
+            }
+
+        if current_status is TicketStatus.RESOLVED:
+            return {
+                TicketStatus.CLOSED,
+                TicketStatus.IN_PROGRESS,
+            }
+
+        return set()
