30b7b28109b92fd2e17fa22523c8eab28c8309f1
Galymzhan Ayapbergen <gaayapbergen@gmail.com>
2026-08-28 16:34:52 +0500

    feat: add ticket comment database model

--- backend/app/database/models.py
+++ backend/app/database/models.py
@@ -1,3 +1,4 @@
+from app.comments.model import TicketComment
 from app.database.base import Base
 from app.tickets.model import Ticket, TicketPriority, TicketStatus
 from app.users.model import User, UserRole
@@ -5,6 +6,7 @@ from app.users.model import User, UserRole
 __all__ = [
     "Base",
     "Ticket",
+    "TicketComment",
     "TicketPriority",
     "TicketStatus",
     "User",
